72cfadb70f2727fe82e2127fefa8c3d9cf423b7b
Muukii <muukii.app@gmail.com>
2022-06-17 03:11:09 +0900

    :evergreen_tree: Update

--- Sources/PreviewLogger/Source.swift
+++ Sources/PreviewLogger/Source.swift
@@ -1,18 +1,45 @@
 import SwiftUI
+import os.log
 
-public enum Log {
+public enum PreviewLog {
   
-  static func debug(_ objects: Any...) {
+  public static func debug(_ objects: Any...) {
     let string = objects.map { "\($0)" }.joined(separator: ", ")
-    LogStorage.shared.logs.append("\n")
-    LogStorage.shared.logs.append(string)
+    LogStorage.shared.logs.append(Log.init(type: .debug, body: string))
   }
+   
+  public static func error(_ objects: Any...) {
+    let string = objects.map { "\($0)" }.joined(separator: ", ")
+    LogStorage.shared.logs.append(Log.init(type: .error, body: string))
+  }
+  
+}
+
+struct Log: Identifiable {
+  var id: UUID = .init()
+  var date: Date = .init()
+  
+  var type: OSLogType
+  var body: String
 }
 
 final class LogStorage: ObservableObject {
   static let shared = LogStorage()
   
-  @Published var logs: String = ""
+  @Published var logs: [Log] = []
+}
+
+private enum Static {
+
+  // To support iOS 13
+  static let formatter: DateFormatter = {
+    
+    let dateFormatter = DateFormatter()
+    dateFormatter.dateFormat = "HH:mm:ss.SSSSSS"
+    
+    return dateFormatter
+  }()
+  
 }
 
 public struct LogView: View {
@@ -25,9 +52,46 @@ public struct LogView: View {
   
   public var body: some View {
     ScrollView {
-      TextField("", text: $storage.logs)
-        .disabled(true)
-        .font(.system(size: 9))
+      VStack {
+        ForEach(storage.logs.reversed()) { log in
+          
+          HStack {
+            
+            circle(type: log.type)
+            
+            Text(Static.formatter.string(from: log.date))
+              .font(.system(size: 9))
+              .foregroundColor(.black.opacity(0.8))
+            Text("\(log.body)")
+              .font(.system(size: 9))
+              .foregroundColor(.black)
+            
+            Spacer()
+            
+          }
+          .padding(.vertical, 2)
+          .padding(.horizontal)
+        }
+      }
     }
+
+  }
+  
+  @ViewBuilder
+  private func circle(type: OSLogType) -> some View {
+    switch type {
+    case .debug:
+      circle(color: .gray)
+    case .error:
+      circle(color: .red)
+    default:
+      circle(color: .yellow)
+    }
+  }
+  
+  private func circle(color: Color) -> some View {
+    Circle()
+      .foregroundColor(color)
+      .frame(width: 6, height: 6)
   }
 }
